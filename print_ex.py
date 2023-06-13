222b3a79099772aec7f1cc5ae87a3bebb40cb491
Ірина Заєць <irynazaiets@MacBook-Air-Iryna.local>
2023-06-13 14:40:05 +0200

    Initial commit

---
+++ print_ex.py
@@ -0,0 +1,45 @@
+hell = "Welcome to hell"
+
+name = "rusnia"
+
+home = "home"
+
+print("\n\n\n")
+
+print("{} just do it {}".format(hell, home))
+
+print("\n\n\n")
+
+
+print(f"east or west {home} is best {home} ")
+
+greeting = "My name is {name} {surname} and I'm {age} y.o."
+greeting_f_string = f"My name is {name} {surname} and I'm {age} y.o."
+print(greeting.format(name="Maxim", surname="Kostenko", age=15))
+print(greeting_f_string)
+
+print("\n\n\n")
+
+print("\nThere is no good or evil — only power and those who are too weak to get it")
+
+print("\nЗдоров, пітон")
+
+print("print")
+
+print("there is only BALENCIAGA")
+
+print("Hey Siri")
+
+print('Як умру, то поховайте')
+
+
+
+print("you're a wizard, Harry!")
+
+print("enough")
+
+print("What's up?")
+
+print("Evil is evil, Stregobor. Lesser, greater, middling, it's all the same")
+
+print("i’m pleased to meet you")
